db5654e98c95ff60cc41f9a8efcbacdf2dcf20e0
Sydney McCarter <sydneymccarter@gmail.com>
2024-02-23 21:54:35 -0500

    updated src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
@@ -8,9 +8,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    
+  // </React.StrictMode>
+  <App />
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
