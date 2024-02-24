045f7e7e30ed5962239f10391951983cb9fca22a
Sydney McCarter <sydneymccarter@gmail.com>
2024-02-23 22:15:25 -0500

    changed react back to .StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
@@ -8,10 +8,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-    
-  // </React.StrictMode>
-  <App />
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>
+  
 
 );
 
