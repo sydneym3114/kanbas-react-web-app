2f6bf81809272a8f36f1b968a0325eef627c602f
Sydney McCarter <sydneymccarter@gmail.com>
2024-03-20 01:18:48 -0400

    completed a4 lab

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,8 +1,12 @@
 import {Link, Route, Routes} from "react-router-dom";
 import Assignment3 from "./a3";
 import Nav from "../Nav";
+import Assignment4 from "./a4";
+import { Provider } from "react-redux";
+import store from "./store";
 function Labs() {
     return (
+        <Provider store={store}>
         <div className="container-fluid">
             <h1>Labs</h1>
             <Nav/>
@@ -10,9 +14,10 @@ function Labs() {
             <Link to="/Labs/a4">Assignment 4</Link>
             <Routes>
                 <Route path="/a3/*" element={<Assignment3/>}/>
-                {/* <Route path="/a4" element={<Assignment4/>}/> */}
+                <Route path="/a4" element={<Assignment4/>}/>
             </Routes>
         </div>
+        </Provider>
     );
 }
 export default Labs;
